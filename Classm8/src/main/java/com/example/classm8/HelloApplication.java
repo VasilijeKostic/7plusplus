02857ca080a8dd63210bd2b8181b2c1300a026de
Vasilije <vasilije.kostic2001@gmail.com>
2023-04-08 22:03:05 +0200

    GUI Start

--- Classm8/src/main/java/com/example/classm8/HelloApplication.java
+++ Classm8/src/main/java/com/example/classm8/HelloApplication.java
@@ -2,19 +2,30 @@ package com.example.classm8;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(HelloApplication.class.getResource("hello-view.fxml"));
+            root.setStyle("-fx-background-color: olivedrab;");
+            Scene scene = new Scene(root, 600, 400);
+            Image icon = new Image(HelloApplication.class.getResourceAsStream("/classm8icon.png"));
+            stage.getIcons().add(icon);
+            stage.setTitle("Classm8 Login");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
