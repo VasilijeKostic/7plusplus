6ae1968642e5ecaaadb009d6275d6565a40661c6
VladanKozic <mr20024@alas.matf.bg.ac.rs>
2023-04-08 20:38:45 +0200

    Klase

--- Classm8/src/main/java/com/example/classm8/HelloApplication.java
+++ Classm8/src/main/java/com/example/classm8/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.classm8;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,14 +12,19 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Button button = new Button("Match");
+        StackPane root = new StackPane();
+        root.getChildren().add(button);
+        Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
+
+
+
