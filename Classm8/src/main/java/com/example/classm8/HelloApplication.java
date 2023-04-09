6eb0547c0f26b5d2f123ee1f0a2befd10356bf92
Detira <126510049+Detira@users.noreply.github.com>
2023-04-09 02:28:31 +0200

    Add files via upload

--- Classm8/src/main/java/com/example/classm8/HelloApplication.java
+++ Classm8/src/main/java/com/example/classm8/HelloApplication.java
@@ -2,37 +2,37 @@ package com.example.classm8;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) {
-        try {
-            Parent root = FXMLLoader.load(HelloApplication.class.getResource("hello-view.fxml"));
-            root.setStyle("-fx-background-color: olivedrab;");
-            Scene scene = new Scene(root, 600, 400);
-            Image icon = new Image(HelloApplication.class.getResourceAsStream("/classm8icon.png"));
-            stage.getIcons().add(icon);
-            stage.setTitle("Classm8 Login");
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Communication com = new Communication();
+        //System.out.println(com.getUser("username"));
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            public void run() {
+                System.out.println(com.getUser("username"));
+            }
+        };
+        timer.schedule(task, 0, 100);
+
+        launch();
     }
 }
-
-
-
