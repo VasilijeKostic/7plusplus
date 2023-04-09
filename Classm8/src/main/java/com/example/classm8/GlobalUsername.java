466ed899a44b7cdbd5ca87f2a07d93caa584990f
Detira <126510049+Detira@users.noreply.github.com>
2023-04-09 11:16:18 +0200

    11:16

---
+++ Classm8/src/main/java/com/example/classm8/GlobalUsername.java
@@ -0,0 +1,13 @@
+package com.example.classm8;
+
+public class GlobalUsername {
+    public static String globalUsername = "smoki";
+
+    public GlobalUsername() {
+    }
+
+    public static String getGlobalUsername() {
+        return globalUsername;
+    }
+}
+
