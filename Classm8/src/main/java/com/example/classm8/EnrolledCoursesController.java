0cb824bb0b2b5a9e3abc39bafe6b68649822026b
VladanKozic <mr20024@alas.matf.bg.ac.rs>
2023-04-09 11:06:05 +0200

    Jos jedan dugmic

--- Classm8/src/main/java/com/example/classm8/EnrolledCoursesController.java
+++ Classm8/src/main/java/com/example/classm8/EnrolledCoursesController.java
@@ -2,12 +2,18 @@ package com.example.classm8;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,8 +23,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class EnrolledCoursesController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @FXML
     private Button buttonChat;
@@ -42,8 +53,40 @@ public class EnrolledCoursesController {
 
     @FXML
     void logout(ActionEvent event) {
-
+        try {
+            root = FXMLLoader.load(getClass().getResource("loginPage.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        root.setStyle("-fx-background-color: olivedrab;");
+        Scene scene = new Scene(root, 1200, 900);
+        //Image icon = new Image(HelloApplication.class.getResourceAsStream("/classm8icon.png"));
+        //stage.getIcons().add(icon);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setTitle("Enrolled Courses");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 
+    @FXML
+    void select(MouseEvent event) {
+        try {
+            root = FXMLLoader.load(getClass().getResource("loginPage.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        root.setStyle("-fx-background-color: olivedrab;");
+        Scene scene = new Scene(root, 1200, 900);
+        //Image icon = new Image(HelloApplication.class.getResourceAsStream("/classm8icon.png"));
+        //stage.getIcons().add(icon);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setTitle("Enrolled Courses");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+    
 }
+
 
